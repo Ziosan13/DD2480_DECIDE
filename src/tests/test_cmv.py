cbd894084937bf8bbefdc35970b33972ea33a158
Eloi Dieme <eloidieme@gmail.com>
2024-01-28 13:26:29 +0100

    feat(cmv): add tests for LIC 3
    Introduce 10 tests for lic3 function base logic.
    
    Refs: #6

--- src/tests/test_cmv.py
+++ src/tests/test_cmv.py
@@ -21,5 +21,99 @@ class test_cmv(unittest.TestCase):
         # Initialize Decide instance
         self.cmv = Cmv(numpoints, points, parameters, lcm, puv)
 
+    def test_lic_3(self) -> None:
+        params = self.parameters.copy()
+
+        points = np.array([
+            [1, 1],
+            [1, 5],
+            [4, 1],
+        ])
+
+        num_points = points.shape[0]
+
+        # Test Case 1:
+        # Input:
+        # - AREA1 is a float with value strictly less than 6.0
+        # - points form a 3-4-5 triangle of area equal to 6.0
+        # Expected behavior: LIC 3 is True.
+        params['AREA1'] = 5.9
+        self.assertTrue(Cmv(params, points, num_points).lic3())
+
+        # Test Case 2:
+        # Input:
+        # - AREA1 is a float with value equal to 6.0
+        # - points form a 3-4-5 triangle of area equal to 6.0
+        # Expected behavior: LIC 3 is False.
+        params['AREA1'] = 6.0
+        self.assertFalse(Cmv(params, points, num_points).lic3())
+
+        # Test Case 3:
+        # Input:
+        # - AREA1 is a float with value strictly greater than 6
+        # - points form a 3-4-5 triangle of area equal to 6.0
+        # Expected behavior: LIC 3 is False.
+        params['AREA1'] = 6.1
+        self.assertFalse(Cmv(params, points, num_points).lic3())
+
+        # Test Case 4:
+        # Input:
+        # - AREA1 is an int with value strictly less than 6
+        # - points form a 3-4-5 triangle of area equal to 6.0
+        # Expected behavior: LIC 3 is True.
+        params['AREA1'] = 5
+        self.assertTrue(Cmv(params, points, num_points).lic3())
+
+        # Test Case 5:
+        # Input:
+        # - AREA1 is an int with value equal to 6
+        # - points form a 3-4-5 triangle of area equal to 6.0
+        # Expected behavior: LIC 3 is False.
+        params['AREA1'] = 6
+        self.assertFalse(Cmv(params, points, num_points).lic3())
+
+        # Test Case 6:
+        # Input:
+        # - AREA1 is an int with value strictly greater than 6
+        # - points form a 3-4-5 triangle of area equal to 6.0
+        # Expected behavior: LIC 3 is False.
+        params['AREA1'] = 7
+        self.assertFalse(Cmv(params, points, num_points).lic3())
+
+        # Test Case 7:
+        # Input:
+        # - AREA1 is a float with value equal to 0.0
+        # - points form a 3-4-5 triangle of area equal to 6.0
+        # Expected behavior: LIC 3 is True.
+        params['AREA1'] = 0.0
+        self.assertTrue(Cmv(params, points, num_points).lic3())
+
+        # Test Case 8:
+        # Input:
+        # - AREA1 is a float with value strictly less than 6.0
+        # - points form a 3-4-5 triangle of area equal to 6.0
+        #   and their coordinates are negative
+        # Expected behavior: LIC 3 is True.
+        params['AREA1'] = 5.9
+        self.assertTrue(Cmv(params, -points, num_points).lic3())
+
+        # Test Case 9:
+        # Input:
+        # - AREA1 is a float with value equal to 6.0
+        # - points form a 3-4-5 triangle of area equal to 6.0
+        #   and their coordinates are negative
+        # Expected behavior: LIC 3 is False.
+        params['AREA1'] = 6.0
+        self.assertFalse(Cmv(params, -points, num_points).lic3())
+
+        # Test Case 10:
+        # Input:
+        # - AREA1 is a float with value strictly greater than 6
+        # - points form a 3-4-5 triangle of area equal to 6.0
+        #   and their coordinates are negative
+        # Expected behavior: LIC 3 is False.
+        params['AREA1'] = 6.1
+        self.assertFalse(Cmv(params, -points, num_points).lic3())
+
 if __name__ == '__main__':
     unittest.main()
