4df8ab27d4605d3097b895dc230c6f1d45144efe
flug0 <hugo@u1.se>
2024-01-28 18:23:41 +0100

    Added parameters and fixed call parameters in cmv_test #32

--- src/tests/test_cmv.py
+++ src/tests/test_cmv.py
@@ -9,17 +9,33 @@ class test_cmv(unittest.TestCase):
         parameters = {
             "LENGTH1": 2,
             "RADIUS1": 1,
-            # add others as needed
-            "AREA2": 5
+            "EPSILON": 0.1,
+            "AREA1": 1,
+            "Q_PTS": 1,
+            "QUADS": 1,
+            "DIST": 1,
+            "N_PTS": 1,
+            "K_PTS": 1,
+            "A_PTS": 1,
+            "B_PTS": 1,
+            "C_PTS": 1,
+            "D_PTS": 1,
+            "E_PTS": 1,
+            "F_PTS": 1,
+            "G_PTS": 1,
+            "LENGTH2": 1,
+            "RADIUS2": 1,
+            "AREA2": 1
         }
         rng = np.random.default_rng()
-        numpoints = 10
-        points = rng.random((numpoints,2))
-        lcm = rng.random((15,15))
-        puv = rng.random((15))
+        num_points = 10
+        points = rng.random((num_points,2))
+        # Dont think these are needed
+        #lcm = rng.random((15,15))
+        #puv = rng.random((15))
 
         # Initialize Decide instance
-        self.cmv = Cmv(numpoints, points, parameters, lcm, puv)
+        self.cmv = Cmv(parameters, points, num_points)
 
 if __name__ == '__main__':
     unittest.main()
