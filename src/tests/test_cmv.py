5724e2df3750db0be40662a4d32c9450e5b275d4
lstrange <lstrange@kth.se>
2024-01-29 14:20:54 +0100

    Added tests for LIC1
    Added tests for LIC1 method in cmv class

--- src/tests/test_cmv.py
+++ src/tests/test_cmv.py
@@ -6,7 +6,7 @@ class test_cmv(unittest.TestCase):
 
     def setUp(self):
         # Create sample input for testing, replace if necessary
-        parameters = {
+        self.parameters = {
             "LENGTH1": 2,
             "RADIUS1": 1,
             "EPSILON": 0.1,
@@ -35,7 +35,64 @@ class test_cmv(unittest.TestCase):
         #puv = rng.random((15))
 
         # Initialize Decide instance
-        self.cmv = Cmv(parameters, points, num_points)
+        self.cmv = Cmv(self.parameters, points, num_points)
+
+    def test_lic_1(self):
+        params = self.parameters.copy()
+        points = np.array([[1, 1],[3, 1],[2, 1.5]]) 
+        num_points = points.shape[0]
+
+        # Test Case 1:
+        # Input: 
+        # RADIUS1 is 0.1
+        # The points form a circle with radius r = sqrt(0.5)
+        # Expect: LIC0 is true 
+        params["RADIUS1"] = 0.1
+        self.assertTrue(Cmv(params, points, num_points).lic1())
+
+        # Test Case 2:
+        # Input: 
+        # RADIUS1 is 1
+        # The points form a circle with radius r = sqrt(0.5)
+        # Expect: LIC0 is true 
+        params["RADIUS1"] = 1
+        self.assertFalse(Cmv(params, points, num_points).lic1())
+
+        # Test Case 3:
+        # Input: 
+        # RADIUS1 is 5
+        # The points form a circle with radius r = sqrt(0.5)
+        # Expect: LIC0 is true 
+        params["RADIUS1"] = 5
+        self.assertFalse(Cmv(params, points, num_points).lic1())
+
+        points = np.array([[1, 1],[3, 1],[2, 1.5],[4,4]]) 
+        num_points = points.shape[0]
+
+        # Test Case 4:
+        # Input: 
+        # RADIUS1 is 0.1
+        # The points form a circle with radius r = sqrt(0.5) and one with r=sqrt(6.5)
+        # Expect: LIC0 is true 
+        params["RADIUS1"] = 0.1
+        self.assertTrue(Cmv(params, points, num_points).lic1())
+
+        # Test Case 5:
+        # Input: 
+        # RADIUS1 is 1
+        # The points form a circle with radius r = sqrt(0.5) and one with r=sqrt(6.5)
+        # Expect: LIC0 is true 
+        params["RADIUS1"] = 1
+        self.assertTrue(Cmv(params, points, num_points).lic1())
+
+        # Test Case 5:
+        # Input: 
+        # RADIUS1 is 2
+        # The points form a circle with radius r = sqrt(0.5) and one with r=sqrt(6.5)
+        # Expect: LIC0 is true 
+        params["RADIUS1"] = 2
+        self.assertFalse(Cmv(params, points, num_points).lic1())
+
 
 if __name__ == '__main__':
     unittest.main()
